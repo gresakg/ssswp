0622fd9e27710a98cf860f131a5968c2e2efb1c3
Gregor Gresak <design@gresak.net>
2017-08-10 10:16:19 +0200

    bugfix

--- sssliderwp.php
+++ sssliderwp.php
@@ -7,7 +7,7 @@
  * Author URI:      http://gresak.net
  * Text Domain:     sssliderwp
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.1.1
  *
  * @package         Sssliderwp
  */
